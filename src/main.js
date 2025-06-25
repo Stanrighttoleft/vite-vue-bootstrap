22f3de9a1369e641ab4cdee1d4e86935e665e8f9
stan <moon_saga@yahoo.com.tw>
2025-06-25 21:21:29 +0800

    successfully setup scss compile and structure of scss and css

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 // Import Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 // Optional: Import Bootstrap JS (for modals, tooltips, etc.)
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
