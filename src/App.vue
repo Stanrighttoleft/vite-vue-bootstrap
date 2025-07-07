ac94729911747b16325a8042e2b5b600b983805a
stan <moon_saga@yahoo.com.tw>
2025-07-07 21:51:50 +0800

    try vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import {ref} from 'vue';
+
+const name=ref('john');
+const status=ref('active');
+const tasks=ref(['task one','task two', 'task three']);
+
+const toggleStatus=()=>{
+  if(status.value==='active'){
+    status.value='pending';
+  }else if(status.value==='pending'){
+    status.value='inactive';
+  }else{
+    status.value='active';
+  }
+}
+
 </script>
 
 <template>
@@ -17,8 +33,26 @@ import { RouterLink, RouterView } from 'vue-router'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+      
     </div>
   </header>
+  <div>
+  <br>
+
+      <h1>{{ name }}</h1>
+      <p v-if="status==='active'">user is active</p>
+      <p v-else-if="status==='pending'">user is pending</p>
+      <p v-else>User is inactive</p>
+      <br>
+
+      <h3>Task:</h3>
+      <br>
+      <ul>
+        <li v-for="task in tasks" :key="task">{{ task }}</li>
+      </ul>
+      <br>
+      <button @click="toggleStatus">Change Status</button>
+  </div>
 
   <RouterView />
 </template>
