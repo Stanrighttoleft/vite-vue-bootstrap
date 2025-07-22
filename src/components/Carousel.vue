27b67a3a05b14b055d77fc45adb2d129a56cf185
stan <moon_saga@yahoo.com.tw>
2025-07-22 21:50:00 +0800

    try carousel futnion

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,11 +1,83 @@
 <template>
+    <div class="carousel">
+        <slot :currentSlide="'currentSlide'"></slot>
+
+        <!-- Navigation -->
+         <div v-if="navEnabled" class="navigate" >
+            <div class="toggle-page left-1/5">
+                <i @click="prevSlide">
+
+                </i>
+            </div>
+
+         </div>
+    </div>
   
 </template>
 
-<script>
-export default {
+<script setup>
+import {ref, onMounted} from "vue";
 
+//props
+const props=defineProps({
+    startAutoPlay:{
+        type:Boolean,
+        default:true,
+    },
+    timeout:{
+        type:Number,
+        default:5000,
+    },
+    navigation:{
+        type:Boolean,
+        default:true
+    },
+    pagination:{
+        type:Boolean,
+        default:true
+    }
+
+})
+
+//state
+const currentSlide=ref(1);
+const getSlideCount=ref(0);
+
+const autoPlayEnabled=ref(props.startAutoPlay);
+const timeoutDuration=ref(props.timeout);
+const paginationEnalbed=ref(props.pagination);
+const navEnabled=ref(props.navigation);
+
+//Methods
+const nextSlide=()=>{
+    currentSlide.value=currentSlide.value===getSlideCount.value ? 1 : currentSlide.value+1;
+};
+
+const prevSlide=()=>{
+    currentSlide.value=currentSlide.value===1 ? 1 : currentSlide.value -1;
+};
+const goToSlide=(index)=>{
+    currentSlide.value=index+1;
 }
+
+//autoaply
+const autoPlay=()=>{
+    setInterval(()=>{
+        nextSlide();
+
+    },timeoutDuration.value);
+};
+if(autoPlayEnabled.value){
+    autoPlay();
+}
+
+//get slide count on mount
+onMounted(()=>{
+    getSlideCount.value=document.querySelectorAll(".slide").length;
+})
+    
+
+
 </script>
 
 <style>
